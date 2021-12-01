a1cb5f86a3a9e3b45ffed1a1402d0468b1834ce6
danielmen-mx <92502464+danielmen-mx@users.noreply.github.com>
2021-12-01 14:01:33 -0600

    created to continue the project

---
+++ database/migrations/2021_12_01_191142_create_costumers_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCostumersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('costumers', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('birthday')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('phone')->unique()->nullable();
+            $table->timestamps();
+        });
+
+        Schema::table('sales', function (Blueprint $table) {
+            $table->unsignedBigInteger('costumer_id')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('costumer_id')->references('id')->on('costumers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('costumers');
+    }
+}
