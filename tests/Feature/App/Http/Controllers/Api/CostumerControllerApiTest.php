8fad2ee0c75897c3430975f7747c865dc2f6fc4e
danielmen-mx <92502464+danielmen-mx@users.noreply.github.com>
2021-12-15 09:37:29 -0600

    create the link sale to costumer test

--- tests/Feature/App/Http/Controllers/Api/CostumerControllerApiTest.php
+++ tests/Feature/App/Http/Controllers/Api/CostumerControllerApiTest.php
@@ -2,16 +2,30 @@
 
 namespace Tests\Feature\App\Http\Controllers\Api;
 
+use App\Models\Costumer;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CostumerControllerApiTest extends TestCase
 {
-    public function test_index_with_searchbar()
+    public function test_store_create_new_costumer()
     {
-        $response = $this->get('/');
+        $user = User::factory()->create();
+        $costumer = Costumer::factory()->create();
+        $email = time() . '@outlook.com';
 
-        $response->assertStatus(200);
+        $payload = [
+            'first_name' => $costumer->first_name,
+            'last_name' => $costumer->last_name,
+            'birthday' => $costumer->birthday,
+            'email' => $email,
+            'phone' => $costumer->phone
+        ];
+
+        $response = $this->actingAs($user)->json('post', 'costumers', $payload);
+
+        $response->assertSuccessful();
     }
 }
